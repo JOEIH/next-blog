f517528a3b234755b7b33477473f7bbe1290d721
JOEIH <joh73459@gmail.com>
2026-04-14 00:10:28 +0900

    style: 공통 스타일 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,22 @@ const config: Config = {
     extend: {
       fontFamily: {
         sans: ['var(--font-pretendard)', 'sans-serif']
+      },
+      colors: {
+        background: 'var(--color-background)',
+        foreground: 'var(--color-foreground)',
+        primary: 'var(--color-primary)',
+        'primary-foreground': 'var(--color-primary-foreground)',
+        'primary-blue': 'var(--color-primary-blue)',
+        lightGray: 'var(--color-lightGray)',
+        deepGray: 'var(--color-deep-gray)'
+      },
+      fontSize: {
+        xs: '0.75rem', // 12px
+        sm: '0.875rem', // 14px
+        base: '1rem', // 16px
+        large: '1.125rem', // 18px
+        huge: '2.25rem' // 36px
       }
     }
   }
