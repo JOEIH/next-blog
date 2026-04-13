eef2e85dfa14608b6202f13b5c21192b049b3e88
JOEIH <joh73459@gmail.com>
2026-04-14 00:11:33 +0900

    chore: svg 아이콘 사용하기 위해 svgr/webpack 설치 및 적용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,44 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true,
+            replaceAttrValues: {
+              '#000': 'currentColor',
+              '#000000': 'currentColor'
+            }
+          }
+        }
+      ]
+    })
+    return config
+  },
 
-export default nextConfig;
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              icon: true,
+              replaceAttrValues: {
+                '#000': 'currentColor',
+                '#000000': 'currentColor'
+              }
+            }
+          }
+        ],
+        as: '*.js'
+      }
+    }
+  }
+}
+
+export default nextConfig
