089f32f20ebbd30149e1d64942fffb23653ce797
JOEIH <joh73459@gmail.com>
2026-04-14 22:42:02 +0900

    style: 모눈 사이즈 코드 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       lang="en"
       className={`${pretendard.variable} h-full`}
       suppressHydrationWarning>
-      <body className="bg-[linear-gradient(to_right,var(--color-grid-line)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-grid-line),transparent_1px)] bg-size-[24px_24px]">
+      <body className="bg-[linear-gradient(to_right,var(--color-grid-line)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-grid-line),transparent_1px)] bg-[size:24px_24px]">
         <ThemeProvider>
           <Header />
 
