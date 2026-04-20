dd2ff0386d9fb3e0db53488a1a074761f63ee3a1
JOEIH <joh73459@gmail.com>
2026-04-21 04:50:24 +0900

    chore: 임시 파일 생성

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,12 @@
 import Introduction from '@/components/Introduction'
+import { getAllPosts } from '@/lib/github'
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getAllPosts()
   return (
     <div>
       <Introduction />
+      {JSON.stringify(posts)}
     </div>
   )
 }
