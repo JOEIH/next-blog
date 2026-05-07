6df0155b700bd021751c80babdd0a01dc1a1d1ca
JOEIH <joh73459@gmail.com>
2026-05-08 05:22:02 +0900

    Feat: 글 상세 호출 api 작성

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,4 @@
 export const TOKEN = process.env.GITHUB_TOKEN
 export const OWNER = process.env.GITHUB_OWNER
 export const REPO = process.env.GITHUB_REPO
-
 export const BASE_PATH = `https://api.github.com/repos/${OWNER}/${REPO}/contents`
--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,6 +1,7 @@
-import { compileMDX } from 'next-mdx-remote/rsc'
-import { MDXComponents } from '@/components/mdx'
+import { PostMatter } from '@/config/types'
 import { BASE_PATH, TOKEN } from '@/constants'
+import matter from 'gray-matter'
+import readingTime from 'reading-time'
 
 // 글 목록 불러오기
 export const getAllPosts = async () => {
@@ -22,9 +23,44 @@ export const getAllPosts = async () => {
   return files
 }
 
-// 글 상세 불러오기
-export const getPostDetail = async (fileName: string) => {
-  const res = await fetch(`${BASE_PATH}/posts/${fileName}.md`, {
+/* -----글 상세 불러오기-----
+1. 글 상세 api 호출
+2. base64로 인코딩된 MDX 파일 디코딩
+3. gray-matter로 frontmatter와 content 분리
+4. reading-time으로 예상 독서 시간 계산
+5. frontmatter, content, readingMinutes 반환
+*/
+export const getPostDetail = async (category: string, slug: string) => {
+  const res = await fetch(`${BASE_PATH}/posts/${category}/${slug}/content.md`, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`
+    }
+  })
+
+  if (!res.ok) {
+    const errorData = await res.text()
+    console.error(res.status)
+    console.error(errorData)
+    throw new Error('글을 불러오는 중 오류가 발생했습니다.')
+  }
+
+  const repoFileInfo = await res.json()
+  const rawMDX = Buffer.from(repoFileInfo.content, 'base64').toString('utf-8')
+  // gray-matter로 frontmatter와 content 분리
+  const { data, content } = matter(rawMDX)
+  const graymatter = data as PostMatter
+  const readingMinutes = Math.ceil(readingTime(content).minutes)
+
+  return {
+    ...graymatter,
+    content,
+    readingMinutes
+  }
+}
+
+// 카테고리별 글 목록 불러오기
+export const getPostsByCategory = async (category: string) => {
+  const res = await fetch(`${BASE_PATH}/posts/${category}`, {
     headers: {
       Authorization: `Bearer ${TOKEN}`
     }
@@ -37,15 +73,6 @@ export const getPostDetail = async (fileName: string) => {
     return []
   }
 
-  const repoFileInfo = await res.json()
-  const rawMDX = Buffer.from(repoFileInfo.content, 'base64').toString('utf-8')
-  const { frontmatter, content } = await compileMDX({
-    source: rawMDX,
-    components: MDXComponents,
-    options: {
-      parseFrontmatter: true
-    }
-  })
-
-  return { frontmatter, content }
+  const files = await res.json()
+  return files
 }
