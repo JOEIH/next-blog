3b897226343e57eab8fbf22e2f070b4e2a06aa2a
JOEIH <joh73459@gmail.com>
2026-05-09 02:56:18 +0900

    Chore: 임의 파일

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,9 @@
 import Introduction from '@/components/Introduction'
-import { getAllPosts } from '@/lib/github'
 
 export default async function Home() {
-  const posts = await getAllPosts()
   return (
     <div>
       <Introduction />
-      {JSON.stringify(posts)}
     </div>
   )
 }
