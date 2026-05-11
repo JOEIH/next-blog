d579b2bc2efd2082593787db63a6100b78b8c18b
JOEIH <joh73459@gmail.com>
2026-05-12 02:55:34 +0900

    Style: mdx 링크 스타일 정의

---
+++ src/components/mdx/ExternalLink.tsx
@@ -0,0 +1,17 @@
+export default function ExternalLink({
+  href,
+  children
+}: {
+  href: string
+  children: React.ReactNode
+}) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-point underline">
+      {children}
+    </a>
+  )
+}
--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,1 +1,6 @@
-export const MDXComponents = {}
+import { MDXComponents } from 'mdx/types'
+import ExternalLink from './ExternalLink'
+
+export const MdxComponents: MDXComponents = {
+  a: ExternalLink
+}
