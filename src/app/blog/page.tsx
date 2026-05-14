8e8e59f0c45c7b9a53ca7e04d68011497e45f71b
JOEIH <joh73459@gmail.com>
2026-05-15 01:42:47 +0900

    Chore: 태그, 카테고리 아이콘 추가

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,26 @@
+import { Icon } from '@/components/common/Icon'
 import Introduction from '@/components/Introduction'
 
 export default async function Home() {
   return (
     <div>
       <Introduction />
+      <div className="text-caption text-deep-gray flex w-full justify-end gap-3.5">
+        <div className="flex cursor-pointer items-center gap-1 font-light">
+          <Icon
+            name="Tag"
+            size={18}
+          />
+          <span>TAG</span>
+        </div>
+        <div className="flex cursor-pointer items-center gap-1 font-light">
+          <Icon
+            name="Grid"
+            size={18}
+          />
+          <span>CATEGORY</span>
+        </div>
+      </div>
     </div>
   )
 }
